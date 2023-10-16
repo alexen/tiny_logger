61beda0bf7cec56643a912f2d8f566a44e63dc26
alexen <enakaev.aleksey@yandex.ru>
2023-10-16 13:19:50 +0300

    main: Пробная работа с классом Rotator

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <boost/exception/diagnostic_information.hpp>
 
 #include <logger/macro.h>
+#include <logger/rotator.h>
 
 
 using FloatArrN7 = std::array< float, 7 >;
@@ -29,45 +30,72 @@ std::ostream& operator<<( std::ostream& os, const FloatArrN7& arr )
 }
 
 
+void testLogger( char** argv )
+{
+     alexen::tiny_logger::Logger logger{ "./logs" };
+
+     static_assert( alexen::tiny_logger::inner::filename( __FILE__ )
+          == std::string_view{ "main.cpp" } );
+
+     const auto currtime = boost::posix_time::second_clock::local_time();
+     const FloatArrN7 arr = {
+          1.273645273,
+          43.18736141,
+          -13.83456382745,
+          -991.32654121234,
+          9.238173517234,
+          -3.142612341634,
+          34532.2387462817465
+     };
+
+     auto iterations = 50'000u;
+
+     const auto start = std::chrono::steady_clock::now();
+     while( iterations-- )
+     {
+          logger.info() << "This is my very first entry! Remains iterations: " << iterations;
+          logger.debug() << "And this is the second line of text! Current time is " << currtime;
+
+          LOG_INFO( logger ) << "This entry made with new macro! We are here: " << boost::filesystem::path{ argv[ 0 ] };
+          LOG_ERROR( logger ) << "Error entry! Array: " << arr;
+          LOG_DEBUG( logger ) << "Debug debug debug";
+          LOG_WARN( logger ) << "WARNING! This is warning message!";
+     }
+     const auto duration = std::chrono::steady_clock::now() - start;
+     std::cout << "Logger: total records made: " << logger.totalRecords()
+          << " for " << std::chrono::duration_cast< std::chrono::duration< double > >( duration ).count() << " s ("
+          << std::chrono::duration_cast< std::chrono::milliseconds >( duration ).count() << " ms, "
+          << std::chrono::duration_cast< std::chrono::microseconds >( duration ).count() << " us)\n";
+}
+
+
 int main( int argc, char** argv )
 {
      boost::ignore_unused( argc, argv );
      try
      {
-          alexen::tiny_logger::Logger logger{ "./logs" };
+          alexen::tiny_logger::Rotator rotator{ alexen::tiny_logger::makeAppName( argv ), "./logs" };
 
-          static_assert( alexen::tiny_logger::inner::filename( __FILE__ )
-               == std::string_view{ "main.cpp" } );
+          std::list< boost::filesystem::path > names{};
 
-          const auto currtime = boost::posix_time::second_clock::local_time();
-          const FloatArrN7 arr = {
-               1.273645273,
-               43.18736141,
-               -13.83456382745,
-               -991.32654121234,
-               9.238173517234,
-               -3.142612341634,
-               34532.2387462817465
-          };
-
-          auto iterations = 50'000u;
-
-          const auto start = std::chrono::steady_clock::now();
-          while( iterations-- )
+          for( auto i = 0; i < 50; ++i )
           {
-               logger.info() << "This is my very first entry! Remains iterations: " << iterations;
-               logger.debug() << "And this is the second line of text! Current time is " << currtime;
-
-               LOG_INFO( logger ) << "This entry made with new macro! We are here: " << boost::filesystem::path{ argv[ 0 ] };
-               LOG_ERROR( logger ) << "Error entry! Array: " << arr;
-               LOG_DEBUG( logger ) << "Debug debug debug";
-               LOG_WARN( logger ) << "WARNING! This is warning message!";
+               names.push_back( rotator.generateNextLogName() );
           }
-          const auto duration = std::chrono::steady_clock::now() - start;
-          std::cout << "Logger: total records made: " << logger.totalRecords()
-               << " for " << std::chrono::duration_cast< std::chrono::duration< double > >( duration ).count() << " s ("
-               << std::chrono::duration_cast< std::chrono::milliseconds >( duration ).count() << " ms, "
-               << std::chrono::duration_cast< std::chrono::microseconds >( duration ).count() << " us)\n";
+
+          std::cout << "Source:\n";
+          for( auto&& each: names )
+          {
+               std::cout << "Log name: " << each << '\n';
+          }
+
+          names.sort();
+
+          std::cout << "Sorted:\n";
+          for( auto&& each: names )
+          {
+               std::cout << "Log name: " << each << '\n';
+          }
      }
      catch( const std::exception& e )
      {
