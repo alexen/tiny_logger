8df200ec9f3779373371c86add26d8189344db47
alexen <enakaev.aleksey@yandex.ru>
2023-10-17 22:06:35 +0300

    main: Удален ненужный тестовый код

--- main.cpp
+++ main.cpp
@@ -25,65 +25,11 @@
 #include <logger/rotator.h>
 
 
-using FloatArrN7 = std::array< float, 7 >;
-
-
-std::ostream& operator<<( std::ostream& os, const FloatArrN7& arr )
-{
-     std::copy(
-          arr.cbegin(),
-          arr.cend(),
-          std::ostream_iterator< FloatArrN7::value_type >{ os, ", " }
-          );
-     return os;
-}
-
-
-void testLogger( char** argv )
-{
-     alexen::tiny_logger::Logger logger{ alexen::tiny_logger::makeAppName( argv ), "./logs", nullptr };
-
-     static_assert( alexen::tiny_logger::inner::filename( __FILE__ )
-          == std::string_view{ "main.cpp" } );
-
-     const auto currtime = boost::posix_time::second_clock::local_time();
-     const FloatArrN7 arr = {
-          1.273645273,
-          43.18736141,
-          -13.83456382745,
-          -991.32654121234,
-          9.238173517234,
-          -3.142612341634,
-          34532.2387462817465
-     };
-
-     auto iterations = 150'000u;
-
-     const auto start = std::chrono::steady_clock::now();
-     while( iterations-- )
-     {
-          logger.info() << "This is my very first entry! Remains iterations: " << iterations;
-          logger.debug() << "And this is the second line of text! Current time is " << currtime;
-
-          LOG_INFO( logger ) << "This entry made with new macro! We are here: " << boost::filesystem::path{ argv[ 0 ] };
-          LOG_ERROR( logger ) << "Error entry! Array: " << arr;
-          LOG_DEBUG( logger ) << "Debug debug debug";
-          LOG_WARN( logger ) << "WARNING! This is warning message!";
-     }
-     const auto duration = std::chrono::steady_clock::now() - start;
-     std::cout << "Logger: total records made: " << logger.totalRecords()
-          << " for " << std::chrono::duration_cast< std::chrono::duration< double > >( duration ).count() << " s ("
-          << std::chrono::duration_cast< std::chrono::milliseconds >( duration ).count() << " ms, "
-          << std::chrono::duration_cast< std::chrono::microseconds >( duration ).count() << " us)\n";
-}
-
-
 int main( int argc, char** argv )
 {
      boost::ignore_unused( argc, argv );
      try
      {
-          testLogger( argv );
      }
      catch( const std::exception& e )
      {
